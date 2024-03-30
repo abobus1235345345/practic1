79c651587f72e5b86ab4547bf94ca3901ade3b2c
abobus1235345345 <139453779+abobus1235345345@users.noreply.github.com>
2024-03-30 09:19:49 +0300

    Add files via upload

---
+++ SolutionMultyProject/ProjectOne/calendar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectOne
+{
+    internal class calendar
+    {
+        public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+            MessageBox.Show($"Today's date is {now}");
+            Console.ReadLine();
+        }
+
+        static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
